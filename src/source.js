cdcf960e47a286b0d8e6bec0035c6da3f8fff001
crittertracker <nhcrittertracker@gmail.com>
2020-04-30 23:21:03 -0400

    v1.4 changes

--- src/source.js
+++ src/source.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
-  console.log( "ready!" );
+ 
+
+
 // Convert Standard Hours to Military
 var d = new Date();
 // console.log(localdate); *debugging
@@ -94,16 +96,20 @@ $.ajax({
   cache: false,
   success: function (data) {
     var fishCounter = 0;
-    $(data.fish).each(function (index, fish) {
+    $(data.fish).each(function (_index, fish) {
+
       if (fish.time.indexOf(h) > -1 && fish.month.indexOf(sMonth) > -1) {
+        
         fishCounter++;
+        
+       var infoIcon = '<svg class="bi bi-info-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/><path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/><circle cx="8" cy="4.5" r="1"/></svg>'
 
         var tableRow =
           '<tr><td id="fName"><b>' +
           (fish.name).toUpperCase() + '</b><br /><img src=' +
           fish.image +
-          ' class="img-fluid" alt="Responsive image"><hr />' +
-          (fish.rarity).toLowerCase() +
+          ' class="img-fluid" alt="Responsive image"><br /><a href="https://animalcrossing.fandom.com/wiki/' + (fish.name) +'" target="_blank" class="btn btn-info my-1" role="button">'+infoIcon+'</a><hr />' +
+          (fish.rarity).toUpperCase() +
           '</td><td>' +
           fish.price +
           '</td><td>' +
@@ -111,16 +117,24 @@ $.ajax({
           '</td><td>' +
           (fish.shadowsize).toUpperCase() +
           '</td></tr>';
-        $(tableRow).appendTo('#userdatafish_nh tbody');
-        
-      }
-      $('#fishCounter').text(fishCounter);
 
-    }); 
+        $(tableRow).appendTo('#userdatafish_nh tbody');
+
+      }
+      $('#fishCounter').text(fishCounter); 
+    });
   },
 });
+//
+
+
+
+
+
+
 
 //AJAX JSON call fish southern
+
 
 $.ajax({
   url: 'src/critters_sh.json',
@@ -129,16 +143,20 @@ $.ajax({
   cache: false,
   success: function (data) {
     var fishCounter = 0;
-    $(data.fish).each(function (index, fish) {
+    $(data.fish).each(function (_index, fish) {
+
       if (fish.time.indexOf(h) > -1 && fish.month.indexOf(sMonth) > -1) {
+        
         fishCounter++;
+        
+       var infoIcon = '<svg class="bi bi-info-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/><path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/><circle cx="8" cy="4.5" r="1"/></svg>'
 
         var tableRow =
           '<tr><td id="fName"><b>' +
           (fish.name).toUpperCase() + '</b><br /><img src=' +
           fish.image +
-          ' class="img-fluid" alt="responsive image"><hr />' +
-          (fish.rarity).toLowerCase() +
+          ' class="img-fluid" alt="Responsive image"><br /><a href="https://animalcrossing.fandom.com/wiki/' + (fish.name.toLowerCase()) +'" target="_blank" class="btn btn-info my-1" role="button">'+infoIcon+'</a><hr />' +
+          (fish.rarity).toUpperCase() +
           '</td><td>' +
           fish.price +
           '</td><td>' +
@@ -146,19 +164,19 @@ $.ajax({
           '</td><td>' +
           (fish.shadowsize).toUpperCase() +
           '</td></tr>';
-        $(tableRow).appendTo('#userdatafish_sh tbody');
-        
-      }
-      $('#fishCounter').text(fishCounter);
 
-    }); 
+        $(tableRow).appendTo('#userdatafish_sh tbody');
+
+      }
+      $('#fishCounter').text(fishCounter); 
+    });
   },
 });
+//
 
 
 
 //AJAX JSON call bugs northern
-
 $.ajax({
   url: 'src/critters_nh.json',
   dataType: 'json',
@@ -166,18 +184,31 @@ $.ajax({
   cache: false,
   success: function (data) {
     var bugCounter = 0;
-    $(data.bugs).each(function (index, bug) {
+    $(data.bugs).each(function (_index, bug) {
+
       if (bug.time.indexOf(h) > -1 && bug.month.indexOf(sMonth) > -1) {
-        bugCounter ++;
+        
+        bugCounter++;
+        
+       var infoIcon = '<svg class="bi bi-info-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/><path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/><circle cx="8" cy="4.5" r="1"/></svg>'
 
         var tableRow =
-          `<tr><td id="bName"><b>${(bug.name).toUpperCase()}</b><br /><img src=${bug.image} class="img-fluid" alt="responsive image"><hr />${(bug.rarity).toLowerCase()}</td><td>${bug.price}</td><td>${(bug.location).toUpperCase()}</td></tr>`;
-        $(tableRow).appendTo('#userdatabugs_nh tbody');
-        
-      }
-      $('#bugCounter').text(bugCounter);
+          '<tr><td id="bName"><b>' +
+          (bug.name).toUpperCase() + '</b><br /><img src=' +
+          bug.image +
+          ' class="img-fluid" alt="Responsive image"><br /><a href="https://animalcrossing.fandom.com/wiki/' + (bug.name.toLowerCase()) +'" target="_blank" class="btn btn-info my-1" role="button">'+infoIcon+'</a><hr />' +
+          (bug.rarity).toUpperCase() +
+          '</td><td>' +
+          bug.price +
+          '</td><td>' +
+          (bug.location).toUpperCase() +
+          '</td></tr>';
 
-    }); 
+        $(tableRow).appendTo('#userdatabugs_nh tbody');
+
+      }
+      $('#bugCounter').text(bugCounter); 
+    });
   },
 });
 
@@ -191,20 +222,68 @@ $.ajax({
   cache: false,
   success: function (data) {
     var bugCounter = 0;
-    $(data.bugs).each(function (index, bug) {
-      if (bug.time.indexOf(h) > -1 && bug.month.indexOf(sMonth) > -1) {
-        bugCounter ++;
-        var tableRow =
-          `<tr><td id="bName"><b>${(bug.name).toUpperCase()}</b><br /><img src=${bug.image} class="img-fluid" alt="responsive image"><hr />${(bug.rarity).toLowerCase()}</td><td>${bug.price}</td><td>${(bug.location).toUpperCase()}</td></tr>`;
-        $(tableRow).appendTo('#userdatabugs_sh tbody');
-        
-      }
-      $('#bugCounter').text(bugCounter);
+    $(data.bugs).each(function (_index, bug) {
 
-    }); 
+      if (bug.time.indexOf(h) > -1 && bug.month.indexOf(sMonth) > -1) {
+        
+        bugCounter++;
+        
+       var infoIcon = '<svg class="bi bi-info-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/><path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/><circle cx="8" cy="4.5" r="1"/></svg>'
+
+        var tableRow =
+          '<tr><td id="bName"><b>' +
+          (bug.name).toUpperCase() + '</b><br /><img src=' +
+          bug.image +
+          ' class="img-fluid" alt="Responsive image"><br /><a href="https://animalcrossing.fandom.com/wiki/' + (bug.name.toLowerCase()) +'" target="_blank" class="btn btn-info my-1" role="button">'+infoIcon+'</a><hr />' +
+          (bug.rarity).toUpperCase() +
+          '</td><td>' +
+          bug.price +
+          '</td><td>' +
+          (bug.location).toUpperCase() +
+          '</td></tr>';
+
+        $(tableRow).appendTo('#userdatabugs_sh tbody');
+
+      }
+      $('#bugCounter').text(bugCounter); 
+    });
   },
-  
 });
+
+
+
+//Filter Functions Fish
+$('#showPier').click(function(){
+  $('td:contains("PIER")').parent().toggle("slow");
+}); 
+
+$('#showPond').click(function(){
+  $('td:contains("POND")').parent().toggle("slow");
+}); 
+
+$('#showRiver').click(function(){
+  $('td:contains("RIVER")').parent().toggle("slow");
+}); 
+
+$('#showSea').click(function(){
+  $('td:contains("SEA")').parent().toggle("slow");
+}); 
+
+
+//Filter Functions Bugs
+$('#showFly').click(function(){
+  $('td:contains("FLYING")').parent().toggle("slow");
+}); 
+
+$('#showGround').click(function(){
+  $('td:contains("GROUND")').parent().toggle("slow");
+}); 
+
+$('#showTree').click(function(){
+  $('td:contains("TREE")').parent().toggle("slow");
+}); 
+
+
 
 
 
@@ -252,6 +331,9 @@ var cTime = convert(mTime);
 $('#currenttime').text(cTime);
 var cDate = d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getFullYear();
 $('#currentdate').text(cDate);
+
+
+
 
 
 //debugging
--- src/timetravel.js
+++ src/timetravel.js
@@ -44,13 +44,15 @@ $.ajax({
     success: function (data) {
       $(data.bugs).each(function (index, bug) {
         if (bug.time.indexOf(hour) > -1 && bug.month.indexOf(month) > -1) {
+
+          var infoIcon = '<svg class="bi bi-info-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/><path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/><circle cx="8" cy="4.5" r="1"/></svg>'
   
           var tableRow =
             '<tr><td><b>' +
             bug.name.toUpperCase() +
             '</b><br /><img src=' +
             bug.image +
-            ' class="img-fluid" alt="responsive image"><hr />' +
+            ' class="img-fluid" alt="responsive image"><br /><a href="https://animalcrossing.fandom.com/wiki/' + (bug.name.toLowerCase()) +'" target="_blank" class="btn btn-info my-1" role="button">'+infoIcon+'</a><hr />' +
             bug.rarity.toLowerCase() +
             '</td><td>' +
             bug.price +
@@ -77,12 +79,14 @@ $.ajax({
       $(data.fish).each(function (index, fish) {
         if (fish.time.indexOf(hour) > -1 && fish.month.indexOf(month) > -1) {
   
+          var infoIcon = '<svg class="bi bi-info-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/><path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/><circle cx="8" cy="4.5" r="1"/></svg>'
+
           var tableRow =
             '<tr><td><b>' +
             fish.name.toUpperCase() +
             '</b><br /><img src=' +
             fish.image +
-            ' class="img-fluid" alt="responsive image"><hr />' +
+            ' class="img-fluid" alt="responsive image"><br /><a href="https://animalcrossing.fandom.com/wiki/' + (fish.name.toLowerCase()) +'" target="_blank" class="btn btn-info my-1" role="button">'+infoIcon+'</a><hr />' +
             fish.rarity.toLowerCase() +
             '</td><td>' +
             fish.price +
@@ -135,7 +139,6 @@ function getCellValue(row, index) {
 
 
 });
-
 
 
 
